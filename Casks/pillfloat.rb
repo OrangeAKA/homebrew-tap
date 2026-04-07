d08d95e8f935fe2d2d22e20592253b47c985072d
Krishna Akhil <akhilsprojectworks@hotmail.com>
2026-04-07 05:43:06 +0530

    Update pillfloat to v1.0.3

--- Casks/pillfloat.rb
+++ Casks/pillfloat.rb
@@ -1,6 +1,6 @@
 cask "pillfloat" do
-  version "1.0.2"
-  sha256 "aabb0be956d1e86923024a178e33f5e8597cbfd26f76bbe57e448c2af685c194"
+  version "1.0.3"
+  sha256 "628bc5f8bcfe6f8eaa84a20923f59839c542300545a685f0209f9eae505258ec"
 
   url "https://github.com/OrangeAKA/pillfloat/releases/download/v#{version}/PillFloat.app.zip"
   name "PillFloat"
